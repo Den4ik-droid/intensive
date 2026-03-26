ac7883691a8fc6261ac25f98256049faba3f631f
Den4ik-droid <denisiik2015@gmail.com>
2026-03-27 01:12:06 +0300

    fourth

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,18 @@
+import { NavLink } from 'react-router-dom';
 import classes from './Header.module.scss';
 export const Header = () => {
+    const getActivateClass = ({ isActive }: { isActive: boolean }): string => {
+        return isActive ? `${classes.active} ${classes.link}` : classes.link;
+    };
     return (
         <header className={classes.header}>
             <div className={classes.conteiner}>
-                <a href="/" className={classes.link}>
-                    ToDo
-                </a>
+                <NavLink to="/" className={getActivateClass}>
+                    Home
+                </NavLink>
+                <NavLink to="/todo" className={getActivateClass}>
+                    Todo
+                </NavLink>
             </div>
         </header>
     );
